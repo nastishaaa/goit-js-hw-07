4f8ec6a822bc0512e1fc0e29bce60217013ef2df
Anastasiia <anastasiiprfnk@gmail.com>
2024-12-18 15:39:57 +0200

    6 task

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn  = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+
+btn.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = getRandomHexColor();
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('input');
+
+const btnC = document.querySelector('[data-create]');
+const btnD = document.querySelector('[data-destroy]');
+
+btnC.addEventListener('click', event => {
+  event.preventDefault();
+  
+  const num = parseInt(input.value);
+
+  if(num < 0 || num > 100){
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+  createBoxes(num);
+  input.value = '';
+})
+
+btnD.addEventListener('click', destroyBoxes);
+
+function createBoxes(num){
+  const fragment = document.createDocumentFragment();
+
+  for(let i = 0; i < num; i++){
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+  }
+
+  boxes.innerHTML = '';
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes(){
+  boxes.innerHTML = '';
+}
